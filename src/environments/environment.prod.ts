13bf9b7c1c72469a2ab67965665459e015e3cfdb
nowlsantos <nowlsantos@gmail.com>
2018-03-05 02:01:33 +0800

    FIrsst commit of environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCIUxeZH-yaIsV0066FlxuJpfjU6m7ZejE',
+    authDomain: 'nowlshoppe.firebaseapp.com',
+    databaseURL: 'https://nowlshoppe.firebaseio.com',
+    projectId: 'nowlshoppe',
+    storageBucket: 'nowlshoppe.appspot.com',
+    messagingSenderId: '346640108205'
+  }
+};
